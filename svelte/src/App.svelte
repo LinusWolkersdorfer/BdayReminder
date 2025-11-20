905a83d0c452a6253228f8874948af79898bbee2
Linus Felix Wolkersdorfer <linus.felix.wolkersdorfer@stud.uni-hannover.de>
2025-11-20 19:42:26 +0100

    Layout now used

--- svelte/src/App.svelte
+++ svelte/src/App.svelte
@@ -1,6 +1,7 @@
 <!-- src/App.svelte -->
 <script>
   import Router from 'svelte-spa-router';
+  import Layout from './Layout.svelte';
   import Home from './routes/Home.svelte';
   import Page1 from './routes/Page1.svelte';
   import Page2 from './routes/Page2.svelte';
@@ -12,4 +13,6 @@
   }
 </script>
 
-<Router {routes} />
+<Layout>
+  <Router {routes} />
+</Layout>
